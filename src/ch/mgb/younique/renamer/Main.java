69fb38f042755cb2f4ad740ec96144da25579224
Timon Hüppi <timon.hueppi@gmail.com>
2021-04-21 14:35:11 +0200

    Adds Main layout

--- src/ch/mgb/younique/renamer/Main.java
+++ src/ch/mgb/younique/renamer/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/MainView.fxml"));
         primaryStage.setTitle("Renamer");
-        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 

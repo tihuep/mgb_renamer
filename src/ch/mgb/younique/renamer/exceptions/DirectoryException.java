b66fb7d6c11abc5320a5b3411d8ca203725d7ef6
Timon Hüppi <timon.hueppi@gmail.com>
2021-04-23 15:52:02 +0200

    Adds custom Exceptions

---
+++ src/ch/mgb/younique/renamer/exceptions/DirectoryException.java
@@ -0,0 +1,7 @@
+package ch.mgb.younique.renamer.exceptions;
+
+public class DirectoryException extends Exception{
+    public DirectoryException(String message) {
+        super(message);
+    }
+}
---
+++ src/ch/mgb/younique/renamer/exceptions/ExcelException.java
@@ -0,0 +1,7 @@
+package ch.mgb.younique.renamer.exceptions;
+
+public class ExcelException extends Exception{
+    public ExcelException(String message) {
+        super(message);
+    }
+}
